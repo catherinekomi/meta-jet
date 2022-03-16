ad46de6bde53449e05ea8c3e6d1f1881d68a6183
Catherine <oceanbeaats@gmail.com>
2022-03-15 18:58:02 -0700

    RoadMap added

--- src/components/roadmap/RoadMap.jsx
+++ src/components/roadmap/RoadMap.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import './roadmap.css'
 
 function RoadMap() {
   return (
-    <div>RoadMap</div>
+    <div class="main">
+        <h3 class="heading">METAJET ROADMAP</h3>
+        <div class="roadcontainer">
+            <ul>
+                <li>
+                    <h3>vip event giveaway</h3>
+                    <p>community choice sporting event givaway, with 1:1 metajet merchandise piece from our fashion designer.</p>
+                    <span class="circle"></span>
+                    <span class="date">20%</span>
+                </li>
+                <li>
+                    <h3 class="title">community choice charity</h3>
+                    <p>our crewpass holders will take a poll for a donation to a charity of our holders choice</p>
+                    <span class="circle"></span>
+                    <span class="date">40%</span>
+                </li>
+                <li>
+                    <h3 class="title">designer merch collection zero</h3>
+                    <p>A collection of 500 designer metajet clothing pieces. These will be designed by our own fashion designer OutKrigh. Once all 500 pieces are claimed by crewpass holders, they will never be available again. Must own a CrewPass claim.</p>
+                    <a href="#">Read More</a>
+                    <span class="circle"></span>
+                    <span class="date">60%</span>
+                </li>
+                <li>
+                    <h3 class="title">first ETH Airdrop</h3>
+                    <p>At the end of every month, 25% of all royalities made from secondary sales will be distributed back to crewpass holders. A snapshot will be taken at the end of every month for distribution.</p>
+                    <span class="circle"></span>
+                    <span class="date">80%</span>
+                </li>
+                <li>
+                    <h3 class="title">phase one commences</h3>
+                    <p>phase one roadmap will begin production. Collection one will be launched with a goal of 2-3 months after 100% sellout of the Crew Pass. MetaJet is here for the future! </p>
+                    <span class="circle"></span>
+                    <span class="date">100%</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+
   )
 }
 
--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -122,6 +122,28 @@ function Team() {
       </ul>
       </div>
 
+      <div className="card">
+      <div className="content">
+      <div className="imgBx">
+        <img src={User} alt="" />
+      </div>
+      <div className="contentBx">
+        <h3>Cole Lander <br/><span>CEO</span></h3>
+      </div>
+      </div>
+      <ul className='sci'>
+        <li className='1'>
+       <SocialIcon url="https://linkedin.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
+        </li>
+         <li className='2'>
+       <SocialIcon url="https://instagram.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
+        </li>
+         <li className='3'> 
+       <SocialIcon url="https://discord.com/" bgColor="#fff" style={{ height: 40, width: 40 }}/>
+        </li>
+      </ul>
+      </div>
+
       </div>
     </section>
   )
